c2980c8781e36d72364b77965e7a3e804895a437
Marcel Enguehard <marcel.enguehard@polyconseil.fr>
2025-11-24 16:26:56 +0100

    Refacto

--- secret_santa/secret_santa/gift_graph.py
+++ secret_santa/secret_santa/gift_graph.py
@@ -49,18 +49,25 @@ class NGiftGraph:
             self.assignments = defaultdict(set)
             flow_graph = self._build_flow_graph()
             flow = flow_graph.compute_largest_flow()
-            is_correct_flow = True
-            for src in flow.graph:
-                if not src == "src":
-                    for dst in flow.graph[src]:
-                        if not dst == "sink":
-                            self.assignments[src.player].add(dst.player)
-                            if not self.allow_2cycles and src.player in self.assignments[dst.player]:
-                                is_correct_flow = False
-                                break
+            is_correct_flow = self._extract_assignments_from_flow(flow)
         
         if not is_correct_flow:
             raise GiftAssignmentError(f"Could not find a valid solution after {self.max_attempts} attempts")
+
+    def _extract_assignments_from_flow(self, flow) -> bool:
+        for src in flow.graph:
+            if src == "src":
+                continue
+            for dst in flow.graph[src]:
+                if dst == "sink":
+                    continue
+                
+                self.assignments[src.player].add(dst.player)
+                if not self.allow_2cycles and src.player in self.assignments[dst.player]:
+                    return False
+        return True
+        
+
 
     def _build_flow_graph(self) -> FlowGraph:
         graph_source = "src"
